bdfa9eb25f0e704fc9ba96243c13d49bca6c1509
sobia-shah <sobiy58@gmail.com>
2024-09-01 00:53:11 +0500

    Add files via upload

---
+++ assignmnt4/app.js
@@ -0,0 +1,121 @@
+// question Number 1
+
+// var cityName = prompt ("please enter the name of your city");
+// if(cityName === "karachi"){
+//     document.write("welcome to th city of lights");
+// }
+
+// question number 2
+
+// var yourGender = prompt ("please enter your gender");
+// if(yourGender === "male"){
+//     document.write("Good Morning Sir.");
+// }
+// else{
+//     document.write("Good Morning Ma'am.");
+// }
+
+// question number 3 
+// var signalColor = prompt("Enternthe color of road traffic signal(red,yellow,green)");
+// if(signalColor === "red"){
+//     document.write("Must Stop")
+// }
+// if(signalColor == "yellow"){
+//     document.write("Ready to Move")
+// }
+// if(signalColor == "green"){
+//     document.write("Move Now")
+// }
+
+// question number 4 
+//  var fuelAmount = +prompt("Enter the remaining fuel in your car (in liters): ")     
+//         if (fuelAmount < 0.25){
+//             alert("Please refill the fuel in your car")}
+//         else{
+//             alert("Fuel level is sufficient")
+//         }
+// question number 5
+// 5(a)
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// // 5(b)
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// 5(c)
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// 5(d)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// 5(e)
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// 5(f)
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// question number 6
+
+
+// var gradeRemarks = (marks_obtained, total_marks)
+// var percentage = (marks_obtained / total_marks) * 100
+
+// if( percentage >= 80){
+//     document.write
+//     (grade = 'A-one'
+//     ,remarks = 'Excellent')}
+// if (percentage >= 70)
+//     { document.write(grade = 'A'
+//     ,remarks = 'Good')}
+// if (percentage >= 60)
+//     { document.write(grade = 'B'
+//     ,remarks = 'You need to improve')}
+// else 
+//  { document.write (grade = 'Fail'
+//       ,remarks = 'Sorry')
+//  }
+
+//  return percentage, grade, remarks
+
+
+//          subject1_marks = prompt("Enter marks obtained in subject 1")
+//          subject2_marks = prompt("Enter marks obtained in subject 2")
+//          subject3_marks = prompt("Enter marks obtained in subject 3")
+//          total_marks = prompt("Enter total marks: ")
+ 
+//         //  Calculating total obtained marks
+//          total_obtained_marks = subject1_marks + subject2_marks + subject3_marks
+ 
+//         //  Calculating percentage, grade and remarks
+//          percentage, grade, remarks = calculate_grade_and_remarks(total_obtained_marks, total_marks)
+ 
+//         //   Displaying the results
+//         document.write("nTotal obtained marks: {total_obtained_marks}")
+//         document.write("Total marks: {total_marks}")
+//         document.writet("Percentage: {percentage:.2f}%")
+//         document.write("Grade: {grade}")
+//         document.write("Remarks: {remarks}")
+// try kia but nah huwa question number 6 or iske bad wale
